24d5c82fafdd1ea77862938ace7e2c747c7585da
karthikeyan <karthikeyan@bangthetable.com>
2021-07-16 21:07:29 +0530

    feat: adds the canonical url for better SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,16 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-
+import { useRouter } from 'next/router'
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
+   const site = "https://karthi.me";
+   const canonicalURL = site + useRouter().pathname
+
   return (
     <>
       <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
+        <link rel ="canonical"  href={canonicalURL}/>
         <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
